53368bf575f86f412558becea44169090c8f1b2d
KaiYuanBu <kaiyuanbu@gmail.com>
2024-11-21 11:15:49 +0800

    test2 works well but once extended unable to retract. test1 unable to extend properly.

--- test1.py
+++ test1.py
@@ -3,9 +3,11 @@ import time
 
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600)
 ser.rts = True  # Relay ON
+# ser.dsr = True
 
-time.sleep(5)
+time.sleep(15)
 ser.rts = False  # Relay OFF
+# ser.dsr = False
 
-time.sleep(5)
+time.sleep(15)
 ser.close()
--- test2.cpp
+++ test2.cpp
@@ -13,11 +13,12 @@ int main() {
         // Turn RTS ON
         serialPort.SetRTS(true);
         std::cout << "Relay ON" << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::this_thread::sleep_for(std::chrono::seconds(15));
 
         // Turn RTS OFF
         serialPort.SetRTS(false);
         std::cout << "Relay OFF" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(15));
 
         serialPort.Close();
     } catch (const std::exception& e) {
@@ -29,5 +30,5 @@ int main() {
 
 
 //RUN THIS --> g++ -o test2 test2.cpp -lserial
-//THEN THIS --> 
+//THEN THIS --> ./test2
 
